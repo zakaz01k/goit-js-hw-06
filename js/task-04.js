e811b0b2cdf351934bc80996901fb27b6e8a9d99
denys <dedpro88@gmail.com>
2023-06-14 23:22:49 +0200

    after mistakes

--- js/task-04.js
+++ js/task-04.js
@@ -1,13 +1,15 @@
-const counterValue = 0; // Початкове значення лічильника
+let counterValue = 0; // Початкове значення лічильника
 
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
 const valueSpan = document.getElementById("value");
 
 decrementBtn.addEventListener("click", () => {
-  valueSpan.textContent = counterValue - 1; // Зменшуємо значення лічильника на 1
+  counterValue -= 1;
+  valueSpan.textContent = counterValue; // Зменшуємо значення лічильника на 1
 });
 
 incrementBtn.addEventListener("click", () => {
-  valueSpan.textContent = counterValue + 1; // Збільшуємо значення лічильника на 1
+  counterValue += 1;
+  valueSpan.textContent = counterValue; // Збільшуємо значення лічильника на 1
 });
